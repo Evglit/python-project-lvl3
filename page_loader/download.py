c0edc6dd04d71f5589677a24ae4261fa0c40517e
Evgeny <litun-07@mail.ru>
2021-04-14 22:49:14 +0300

    step 1.3

--- page_loader/download.py
+++ page_loader/download.py
@@ -3,18 +3,21 @@
 
 import requests
 import re
+import os
 
 
-def download(url, path_output):
+def download(url, path_output=os.getcwd()):
     page = requests.get(url)
-    path_file = get_path_file(url, path_output)
-    file = open(path_file, 'w')
-    file.write(page.text)
-    return path_file
+    if page.status_code == 200:
+        path_file = get_path_file(url, path_output)
+        with open(path_file, 'w') as file:
+            file.write(page.text)
+        return path_file
+    return f'Error: {page.status_code}'
 
 
 def get_path_file(url, path_output):
     file_name = re.sub(r'^https:\/\/', r'', url)
     file_name = re.sub(r'\W', r'-', file_name) + '.html'
-    path_file = path_output + '/' + file_name
+    path_file = os.path.join(path_output, file_name)
     return path_file
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,7 +1,18 @@
 """Test for Page loader"""
 
-from page_loader.download import download
+from page_loader import download
+import requests
+import requests_mock
+from pathlib import Path
+from bs4 import BeautifulSoup
 
 
-def test_downlosd():
-    assert download('https://ru.hexlet.io/courses', '/var/tmp') == '/var/tmp/ru-hexlet-io-courses.html'
+def test_download(requests_mock):
+    path_right_page = Path(Path(__file__).parent.absolute() / 'fixtures' / 'Habr regular expressions.html')
+    with open(path_right_page, 'r') as file:
+        right_page = file.read()
+    requests_mock.get('https://habr.com/ru/post/349860', text=right_page)
+    path_downloaded_page = download('https://habr.com/ru/post/349860')
+    with open(path_downloaded_page, 'r') as file:
+        downloaded_page = file.read()
+    assert right_page == downloaded_page
