abc774b68ea44064245b38b2ba334038692745e1
Evgeny <litun-07@mail.ru>
2021-04-20 11:49:45 +0300

    Step 2.1

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,6 +1,6 @@
 """Page loader"""
 
-
+from bs4 import BeautifulSoup
 import requests
 import re
 import os
@@ -9,15 +9,34 @@ import os
 def download(url, path_output=os.getcwd()):
     page = requests.get(url)
     if page.status_code == 200:
-        path_file = get_path_file(url, path_output)
-        with open(path_file, 'w') as file:
-            file.write(page.text)
-        return path_file
-    return f'Error: {page.status_code}'
+        local_name = get_local_name(url)
+        local_path_files = os.path.join(path_output, local_name) + '_files'
+        if not os.path.exists(local_path_files):
+            os.mkdir(local_path_files)
+        page_html = download_imges(page.text, local_path_files)
+        local_path_page = os.path.join(path_output, local_name) + '.html'
+        with open(local_path_page, 'w') as file:
+            file.write(page_html)
+        return local_path_page
+    raise f'Error: {page.status_code}'
 
 
-def get_path_file(url, path_output):
-    file_name = re.sub(r'^https:\/\/', r'', url)
-    file_name = re.sub(r'\W', r'-', file_name) + '.html'
-    path_file = os.path.join(path_output, file_name)
-    return path_file
+def get_local_name(url):
+    local_name = re.sub(r'^https:\/\/', r'', url)
+    local_name = re.sub(r'\W', r'-', os.path.splitext(local_name)[0])\
+        + os.path.splitext(local_name)[1]
+    return local_name
+
+
+def download_imges(page_html, local_path):
+    soup = BeautifulSoup(page_html, "lxml")
+    imgs = soup.find_all('img')
+    for img in imgs:
+        url_img = img.get('src')
+        img_file = requests.get(url_img)
+        img_name = os.path.basename(url_img)
+        local_path_img = os.path.join(local_path, get_local_name(img_name))
+        with open(local_path_img, 'wb') as file:
+            file.write(img_file.content)
+        page_html = re.sub(url_img, os.path.relpath(local_path_img), page_html)
+    return page_html
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -8,11 +8,11 @@ from bs4 import BeautifulSoup
 
 
 def test_download(requests_mock):
-    path_right_page = Path(Path(__file__).parent.absolute() / 'fixtures' / 'Habr regular expressions.html')
+    path_right_page = Path(Path(__file__).parent.absolute() / 'fixtures' / 'parislife-ru-blog_files.html')
     with open(path_right_page, 'r') as file:
         right_page = file.read()
-    requests_mock.get('https://habr.com/ru/post/349860', text=right_page)
-    path_downloaded_page = download('https://habr.com/ru/post/349860')
+    requests_mock.get('https://parislife.ru/blog', text=right_page)
+    path_downloaded_page = download('https://parislife.ru/blog')
     with open(path_downloaded_page, 'r') as file:
         downloaded_page = file.read()
     assert right_page == downloaded_page
