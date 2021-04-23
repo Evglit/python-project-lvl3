5324dc27cd239488a292ff65518ea8cff92fb78d
Evgeny <litun-07@mail.ru>
2021-04-23 16:56:54 +0300

    Step 3.1

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,42 +1,71 @@
 """Page loader"""
 
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 import requests
 import re
 import os
 
 
+RESOURCES = {
+    'img': 'src',
+    'link': 'href',
+    'script': 'src',
+}
+
+
 def download(url, path_output=os.getcwd()):
+    url = url.rstrip('/')
     page = requests.get(url)
-    if page.status_code == 200:
-        local_name = get_local_name(url)
-        local_path_files = os.path.join(path_output, local_name) + '_files'
-        if not os.path.exists(local_path_files):
-            os.mkdir(local_path_files)
-        page_html = download_imges(page.text, local_path_files)
-        local_path_page = os.path.join(path_output, local_name) + '.html'
-        with open(local_path_page, 'w') as file:
-            file.write(page_html)
-        return local_path_page
-    raise f'Error: {page.status_code}'
+    if page.status_code != 200:
+        raise f'Error{page.status_code}'
+    local_name_page = get_name_page(url)
+    local_path_page = os.path.join(path_output, local_name_page)
+    save_file(page.text, local_path_page)
+    local_path_files = local_path_page.replace('.html', '_files')
+    if not os.path.exists(local_path_files):
+        os.mkdir(local_path_files)
+    for teg, attribut in RESOURCES.items():
+        download_resources(
+            local_path_page, local_path_files,
+            url, teg, attribut)
+    return local_path_page
 
 
-def get_local_name(url):
-    local_name = re.sub(r'^https:\/\/', r'', url)
-    local_name = re.sub(r'\W', r'-', os.path.splitext(local_name)[0])\
-        + os.path.splitext(local_name)[1]
-    return local_name
+def get_name_page(url):
+    scheme = urlparse(url).scheme
+    name_page = re.sub(f'{scheme}://', r'', url)
+    name_page = re.sub(r'\W', r'-', name_page) + '.html'
+    return name_page
 
 
-def download_imges(page_html, local_path):
-    soup = BeautifulSoup(page_html, "lxml")
-    imgs = soup.find_all('img')
-    for img in imgs:
-        url_img = img.get('src')
-        img_file = requests.get(url_img)
-        img_name = os.path.basename(url_img)
-        local_path_img = os.path.join(local_path, get_local_name(img_name))
-        with open(local_path_img, 'wb') as file:
-            file.write(img_file.content)
-        page_html = re.sub(url_img, os.path.relpath(local_path_img), page_html)
-    return page_html
+def save_file(content, path, mode='w'):
+    with open(path, mode) as file:
+        file.write(content)
+
+
+def download_resources(path_page, path_files, url_host, tag, attribute):
+    with open(path_page) as file:
+        page_html = file.read()
+    soup = BeautifulSoup(page_html, "html.parser")
+    tags_resource = soup.find_all(tag)
+    for tag_resource in tags_resource:
+        attr_resource = tag_resource.get(attribute)
+        if (urlparse(attr_resource).netloc == urlparse(url_host).netloc or
+                urlparse(attr_resource).netloc == ''):
+            url_resource = urljoin(url_host, attr_resource).rstrip('/')
+            response = requests.get(url_resource)
+            name_resource = get_name_resource(url_resource)
+            local_path_resource = os.path.join(path_files, name_resource)
+            save_file(response.content, local_path_resource, 'wb')
+            tag_resource[attribute] = os.path.relpath(local_path_resource)
+    save_file(soup.prettify(formatter='html5'), path_page)
+
+
+def get_name_resource(url):
+    name_resource = re.sub(r'\?.*$', r'', os.path.basename(url))
+    base, ext = os.path.splitext(name_resource)
+    name_resource = re.sub(r'\W', r'-', base) + ext
+    """if ext == '':
+        name_resource += '.html'"""
+    return name_resource
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,18 +1,16 @@
 """Test for Page loader"""
 
 from page_loader import download
-import requests
-import requests_mock
 from pathlib import Path
-from bs4 import BeautifulSoup
+import requests_mock
 
 
 def test_download(requests_mock):
-    path_right_page = Path(Path(__file__).parent.absolute() / 'fixtures' / 'parislife-ru-blog_files.html')
+    path_right_page = Path(Path(__file__).parent.absolute() / 'fixtures' / 'parislife.html')
     with open(path_right_page, 'r') as file:
         right_page = file.read()
-    requests_mock.get('https://parislife.ru/blog', text=right_page)
-    path_downloaded_page = download('https://parislife.ru/blog')
+    requests_mock.get('https://evglit.github.io', text=right_page)
+    path_downloaded_page = download('https://evglit.github.io/')
     with open(path_downloaded_page, 'r') as file:
         downloaded_page = file.read()
     assert right_page == downloaded_page
