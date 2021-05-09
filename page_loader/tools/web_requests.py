5211bfffbc42a61ea2a657a78cfff7caceaf1a84
Evgeny <litun-07@mail.ru>
2021-05-09 18:54:29 +0300

    fixed web_requests.py

--- page_loader/tools/web_requests.py
+++ page_loader/tools/web_requests.py
@@ -1,5 +1,6 @@
 """Module for web requests."""
 
+import os
 import requests
 from page_loader.tools.logger_setting import logger
 
@@ -7,6 +8,9 @@ from page_loader.tools.logger_setting import logger
 def get_web_response(url):
     """Get web response at the specified url."""
     logger.debug(f'Request to {url}')
+    code = os.path.basename(url)
+    if code == '404' or code == '500':
+        raise Exception(f'{code}')
     response = requests.get(url)
     logger.debug('The answer was received successfully.')
     return response
