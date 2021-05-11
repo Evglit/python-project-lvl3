aa3fc7a79f00052e36444a4ec09875649b8830ba
Evgeny <litun-07@mail.ru>
2021-05-11 23:22:02 +0300

    Revert "changes to logger_setting.py"
    This reverts commit 97b9f0e30e5f0450b5b3980c43970e2eb621307b.

--- page_loader/tools/logger_setting.py
+++ page_loader/tools/logger_setting.py
@@ -1,6 +1,5 @@
 """Setting logging."""
 
-import os
 import logging
 
 
@@ -12,23 +11,20 @@ logger.setLevel(logging.DEBUG)
 
 # Create handlers
 c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler('file.log')
 c_handler.setLevel(logging.WARNING)
+f_handler.setLevel(logging.DEBUG)
 
 # Create formatters and add it to handlers
 c_format = logging.Formatter('%(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
 
 # Add handlers to the logger
 logger.addHandler(c_handler)
+logger.addHandler(f_handler)
 
 
-def config_logger(log_level, path_output):
+def config_logger(log_level):
     c_handler.setLevel(log_level)
-
-    # Create FileHandler
-    log_path = os.path.join(path_output, 'file.log')
-    f_handler = logging.FileHandler(log_path)
-    f_handler.setLevel(logging.DEBUG)
-    f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    f_handler.setFormatter(f_format)
-    logger.addHandler(f_handler)
