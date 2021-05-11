97b9f0e30e5f0450b5b3980c43970e2eb621307b
Evgeny <litun-07@mail.ru>
2021-05-11 23:15:43 +0300

    changes to logger_setting.py

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -10,7 +10,7 @@ from page_loader.tools.logger_setting import logger, config_logger
 def main():
     url, path_output, log_level = parse_arg()
     try:
-        config_logger(log_level)
+        config_logger(log_level, path_output)
         path = download(url, path_output)
         print(f'Page was successfully downloaded into {path}')
         sys.exit(0)
