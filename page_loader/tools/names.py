332a012f7ccb466e2b2eafb9fc888f48c05ef9ea
Evgeny <litun-07@mail.ru>
2021-05-25 21:43:56 +0300

    changed names.py

--- page_loader/tools/names.py
+++ page_loader/tools/names.py
@@ -8,20 +8,20 @@ from page_loader.tools.logger_setting import logger
 
 def get_name_page(url):
     """Get page name from page url."""
-    scheme = urlparse(url).scheme
-    name_page = re.sub(f'{scheme}://', r'', url)
-    name_page = re.sub(r'\W', r'-', name_page) + '.html'
+    name_page = re.sub(r'\W', r'-', delete_scheme(url)) + '.html'
     logger.debug(f'Generated the page name "{name_page}" from URL "{url}"')
     return name_page
 
 
 def get_name_resource(url):
     """Get resource name from resource url."""
-    scheme = urlparse(url).scheme
-    name_resource = re.sub(f'{scheme}://', r'', url)
-    base, ext = os.path.splitext(name_resource)
+    base, ext = os.path.splitext(delete_scheme(url))
     name_resource = re.sub(r'\W', r'-', base) + ext
     if not ext:
         name_resource += '.html'
     logger.debug(f'Generated filename "{name_resource}" from URL "{url}"')
     return name_resource
+
+
+def delete_scheme(url):
+    return urlparse(url).netloc + urlparse(url).path
